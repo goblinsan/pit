66bcdc056de6b77a414fd19a71d10fd972826023
goblinsan <coghlanjames@yahoo.com>
2018-05-27 00:34:08 -0400

    add react router and a custom login form

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Grid, Navbar, Panel, Row} from 'react-bootstrap';
+import {Button, ControlLabel, FormControl, FormGroup, Grid, Navbar, Panel, Row} from 'react-bootstrap';
 import './App.css';
 import Col from "react-bootstrap/es/Col";
 import BidTable from "./Bids";
@@ -8,6 +8,90 @@ import PlayerTable from "./Players";
 import OfferTable from "./Offers";
 import TradeTable from "./Trades";
 import * as DataAccess from "./dataAccess.js";
+import {BrowserRouter as Router, Link, Redirect, Route, withRouter} from "react-router-dom";
+
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+const AuthButton = withRouter(
+    ({ history }) =>
+        fakeAuth.isAuthenticated ? (
+            <p>
+                Welcome!{" "}
+                <button
+                    onClick={() => {
+                        fakeAuth.signout(() => history.push("/"));
+                    }}
+                >
+                    Sign out
+                </button>
+            </p>
+        ) : (
+            <p>You are not logged in.</p>
+        )
+);
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            fakeAuth.isAuthenticated ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+);
+
+const Public = () => <h3>Public</h3>;
+const Chucky = () => <h3>Logged in homie</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+class Login extends React.Component {
+    state = {
+        redirectToReferrer: false
+    };
+
+    login = () => {
+        fakeAuth.authenticate(() => {
+            this.setState({ redirectToReferrer: true });
+        });
+    };
+
+    render() {
+        const { from } = this.props.location.state || { from: { pathname: "/" } };
+        const { redirectToReferrer } = this.state;
+
+        if (redirectToReferrer) {
+            return <Redirect to={from} />;
+        }
+
+        return (
+            <div>
+                <p>You must log in to view the page at {from.pathname}</p>
+                <button onClick={this.login}>Log in</button>
+            </div>
+        );
+    }
+}
+
+
 
 class StartGameButton extends React.Component {
     render() {
@@ -27,13 +111,17 @@ class App extends Component {
         this.state = {
             error: null,
             isLoaded: false,
+            showModal: false,
+            loading: false,
             currentTime: null,
             gameStarted: false,
             schedule: [],
             players: [],
             offers: [],
             bids: [],
-            trades: []
+            trades: [],
+            username: "",
+            password: ""
         };
         this.getGameInfo = this.getGameInfo.bind(this);
         this.startGame = DataAccess.startGame.bind(this);
@@ -58,76 +146,153 @@ class App extends Component {
         this.setState({isLoaded: true});
     }
 
+    validateForm() {
+        return this.state.username.length > 0 && this.state.password.length > 0;
+    }
+
+    onChange = (e) => {
+        this.setState({[e.target.id]: e.target.value});
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        let formData = new FormData();
+        formData.append("username", this.state.username);
+        formData.append("password", this.state.password);
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            body: formData
+        });
+    };
+
     render() {
         return (
-            <div className="App">
-                <Navbar inverse>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            Pit Trading Server
-                        </Navbar.Brand>
-                        <StartGameButton
-                            gameStarted={this.state.gameStarted}
-                            startGame={this.startGame}
-                        />
-                    </Navbar.Header>
-                </Navbar>
+            <Router>
+                <div>
+                    <AuthButton />
+                    <ul>
+                        <li>
+                            <Link to="/public">Public Page</Link>
+                        </li>
+                        <li>
+                            <Link to="/protected">Protected Page</Link>
+                        </li>
+                        <li>
+                            <Link to="/chucky">get me chuck</Link>
+                        </li>
+                    </ul>
+                    <Route path="/public" component={Public} />
+                    {/*<Route path="/login" component={Login} />*/}
+                    <Route path="/chucky" component={Chucky}/>
+                    <PrivateRoute path="/protected" component={Protected} />
 
-                <Grid>
-                    <Panel>
-                        <Panel.Body>
-                            <Row className="show-grid">
-                                <Col md={4} >
-                                    <Panel>
-                                        <Panel.Heading>Schedule</Panel.Heading>
-                                        <Panel.Body>
-                                            <ScheduleTable
-                                                currentTime={this.state.currentTime}
-                                                schedule={this.state.schedule}
-                                                gameStarted={this.state.gameStarted}
-                                            />
-                                        </Panel.Body>
-                                    </Panel>
-                                </Col>
-                                <Col md={4} >
-                                    <Panel>
-                                        <Panel.Heading>Players</Panel.Heading>
-                                        <Panel.Body>
-                                            <PlayerTable players={this.state.players}/>
-                                        </Panel.Body>
-                                    </Panel>
-                                </Col>
-                                <Col md={4} >
-                                    <Panel>
-                                        <Panel.Heading>Offers</Panel.Heading>
-                                        <Panel.Body>
-                                            <OfferTable offers={this.state.offers}/>
-                                        </Panel.Body>
-                                    </Panel>
-                                </Col>
-                            </Row>
-                            <Row className="show-grid">
-                                <Col md={6} >
-                                    <Panel>
-                                        <Panel.Heading>Bids</Panel.Heading>
-                                        <Panel.Body>
-                                            <BidTable bids={this.state.bids}/>
-                                        </Panel.Body>
-                                    </Panel>
-                                </Col>
-                                <Col md={6} >
-                                    <Panel>
-                                        <Panel.Heading>Trades</Panel.Heading>
-                                        <Panel.Body>
-                                            <TradeTable trades={this.state.trades}/>
-                                        </Panel.Body>
-                                    </Panel>
-                                </Col>
-                            </Row>
-                        </Panel.Body>
-                    </Panel>
-                </Grid>
-            </div>
+
+                    <div className="App">
+                        <Navbar inverse>
+                            <Navbar.Header>
+                                <Navbar.Brand>
+                                    Pit Trading Server
+                                </Navbar.Brand>
+                                <StartGameButton
+                                    gameStarted={this.state.gameStarted}
+                                    startGame={this.startGame}
+                                />
+                            </Navbar.Header>
+                        </Navbar>
+
+                        <Grid>
+                            <Panel>
+                                <Panel.Body>
+                                    <Row className="show-grid">
+                                        <Col md={4} >
+                                            <Panel>
+                                                <Panel.Heading>Schedule</Panel.Heading>
+                                                <Panel.Body>
+                                                    <ScheduleTable
+                                                        currentTime={this.state.currentTime}
+                                                        schedule={this.state.schedule}
+                                                        gameStarted={this.state.gameStarted}
+                                                    />
+                                                </Panel.Body>
+                                            </Panel>
+                                        </Col>
+                                        <Col md={4} >
+                                            <Panel>
+                                                <Panel.Heading>Players</Panel.Heading>
+                                                <Panel.Body>
+                                                    <PlayerTable players={this.state.players}/>
+                                                </Panel.Body>
+                                            </Panel>
+                                        </Col>
+                                        <Col md={4} >
+                                            <Panel>
+                                                <Panel.Heading>Offers</Panel.Heading>
+                                                <Panel.Body>
+                                                    <OfferTable offers={this.state.offers}/>
+                                                </Panel.Body>
+                                            </Panel>
+                                        </Col>
+                                    </Row>
+                                    <Row className="show-grid">
+                                        <Col md={6} >
+                                            <Panel>
+                                                <Panel.Heading>Bids</Panel.Heading>
+                                                <Panel.Body>
+                                                    <BidTable bids={this.state.bids}/>
+                                                </Panel.Body>
+                                            </Panel>
+                                        </Col>
+                                        <Col md={6} >
+                                            <Panel>
+                                                <Panel.Heading>Trades</Panel.Heading>
+                                                <Panel.Body>
+                                                    <TradeTable trades={this.state.trades}/>
+                                                </Panel.Body>
+                                            </Panel>
+                                        </Col>
+                                    </Row>
+                                </Panel.Body>
+                            </Panel>
+                        </Grid>
+                    </div>
+
+                    <div id={"formForLogin"}>
+                        <form onSubmit={this.handleSubmit}>
+                            <FormGroup controlId="username" bsSize="large">
+                                <ControlLabel>User</ControlLabel>
+                                <FormControl
+                                    autoFocus
+                                    type="text"
+                                    value={this.state.username}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                            <FormGroup controlId="password" bsSize="large">
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl
+                                    value={this.state.password}
+                                    onChange={this.onChange}
+                                    type="password"
+                                />
+                            </FormGroup>
+                            <Button
+                                block
+                                bsSize="large"
+                                disabled={!this.validateForm()}
+                                type="submit"
+                            >
+                                Login
+                            </Button>
+                        </form>
+                    </div>
+
+                </div>
+
+
+            </Router>
+
+
+
         );
     }
 }
